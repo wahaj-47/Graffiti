44f398692279c8e70705ade4fcaacfb9018cb217
Wahaj Hussain <wahajhssn@gmail.com>
2025-08-21 10:42:58 -0500

    Exporting the class

--- app/graffiti/PaintBrush.ts
+++ app/graffiti/PaintBrush.ts
@@ -2,18 +2,21 @@ import type { FederatedPointerEvent } from "pixi.js";
 import type { Doc } from "yjs";
 import { Brush, type BrushConfig } from "./Brush";
 
-class PaintBrush extends Brush {
+export class PaintBrush extends Brush {
   constructor(config: BrushConfig) {
     super(config);
   }
 
   onPointerDown(e: FederatedPointerEvent, doc: Doc): void {
     // @TODO: Start transaction for a new brush stroke
+    console.log("PaintBrush: Pointer down");
   }
   onPointerMove(e: FederatedPointerEvent, doc: Doc): void {
     // @TODO: Add transactions for each pointer event
+    console.log("PaintBrush: Pointer move");
   }
   onPointerUp(e: FederatedPointerEvent, doc: Doc): void {
     // @TODO: Finish and commit transaction
+    console.log("PaintBrush: Pointer up");
   }
 }

546e7667d76432e8057fca4e8e28a63d69ba5e57
Wahaj Hussain <wahajhssn@gmail.com>
2025-08-21 10:41:33 -0500

    Base Tool interface + BrushProps

--- app/types.ts
+++ app/types.ts
@@ -1,4 +1,12 @@
-type BrushProps = {
-  active: boolean;
-  color: string | number;
+import type { FederatedPointerEvent, PathInstruction } from "pixi.js";
+import type { Doc } from "yjs";
+
+export interface Tool {
+  onPointerDown(e: FederatedPointerEvent, doc: Doc): void;
+  onPointerMove(e: FederatedPointerEvent, doc: Doc): void;
+  onPointerUp(e: FederatedPointerEvent, doc: Doc): void;
+}
+
+export type BrushProps = {
+  instructions: PathInstruction[];
 };

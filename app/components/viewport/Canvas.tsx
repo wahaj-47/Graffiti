3cb79c4f6c77076a3b7f6e8d9f37edeac10dbd76
Wahaj Hussain <wahajhssn@gmail.com>
2025-09-06 16:25:21 -0500

    A canvas component to house the Viewport and Artboard

--- app/components/viewport/Canvas.tsx
+++ app/components/viewport/Canvas.tsx
@@ -1,14 +1,31 @@
-import { Application, extend, useApplication } from "@pixi/react";
-import { Viewport } from "pixi-viewport";
-
-extend({ Viewport });
+import { Application } from "@pixi/react";
+import type { FederatedPointerEvent } from "pixi.js";
+import { useRef } from "react";
+import { useUndoManger, useYDoc } from "~/context/YContext";
+import { PaintBrush } from "~/engine/PaintBrush";
+import type { Tool } from "~/engine/Tool";
+import { useKeyPress } from "~/hooks/useKeys";
+import { GraffitiViewport as Viewport } from "./Viewport";
+import { Artboard } from "../artboard/Artboard";
 
 export function Canvas() {
-  const { app } = useApplication();
+  const doc = useYDoc();
+
+  const tool = useRef<Tool>(new PaintBrush(doc, { radius: 1, color: "red" }));
+  const onPointerDown = (event: FederatedPointerEvent) => tool.current.onPointerDown(event);
+  const onPointerMove = (event: FederatedPointerEvent) => tool.current.onPointerMove(event);
+  const onPointerUp = (event: FederatedPointerEvent) => tool.current.onPointerUp(event);
+  const onPointerLeave = (event: FederatedPointerEvent) => tool.current.onPointerLeave(event);
+
+  const undoManager = useUndoManger();
+  useKeyPress(["ctrl+z"], (e) => undoManager.undo());
+  useKeyPress(["ctrl+shift+z"], (e) => undoManager.redo());
 
   return (
-    <Application>
-      <pixiViewport events={app.renderer.events}></pixiViewport>
+    <Application width={window.innerWidth} height={window.innerHeight} resizeTo={window}>
+      <Viewport>
+        <Artboard></Artboard>
+      </Viewport>
     </Application>
   );
 }

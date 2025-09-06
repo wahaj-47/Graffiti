b50e04aea63aa165d1fb7a6c65b083939f272de3
Wahaj Hussain <wahajhssn@gmail.com>
2025-09-06 16:24:56 -0500

    Simplifying the artboard

--- app/components/artboard/artboard.tsx
+++ app/components/artboard/artboard.tsx
@@ -1,68 +1,37 @@
-import { useRef } from "react";
-import { Array } from "yjs";
-import { Application, extend } from "@pixi/react";
-import { Container, FederatedPointerEvent, Rectangle } from "pixi.js";
+import { extend } from "@pixi/react";
+import { Container, Rectangle } from "pixi.js";
 import { canUseDOM } from "~/client/utils";
-import { PaintBrush } from "~/graffiti/PaintBrush";
-import { type Tool, type ToolConfig } from "~/types";
-import { useUndoManger, useYArray, useYDoc } from "~/context/YContext";
+import { type ToolConfig } from "~/types";
+import { useYArray } from "~/context/YContext";
 import { Brush } from "../tool/Brush";
-import { useKeyPress } from "~/hooks/useKeys";
-
-const WIDTH = 1280;
-const HEIGHT = 720;
-const HIT = new Rectangle(0, 0, WIDTH, HEIGHT);
 
 extend({ Container });
 
 export function Artboard() {
-  const doc = useYDoc();
   const history = useYArray<ToolConfig>("history");
-  const instructions = useYArray<Array<unknown>>("instructions", "none");
-  const undoManager = useUndoManger();
-
-  useKeyPress(["ctrl+z"], (e) => undoManager.undo());
-  useKeyPress(["ctrl+shift+z"], (e) => undoManager.redo());
-
-  const tool = useRef<Tool>(new PaintBrush({ radius: 1, color: "red" }));
-
-  const onPointerDown = (event: FederatedPointerEvent) => {
-    doc.transact(() => {
-      history.push([tool.current.getConfig()]);
-      instructions.push([tool.current.onPointerDown(event)]);
-    });
-  };
-
-  const onPointerMove = (event: FederatedPointerEvent) => {
-    tool.current.onPointerMove(event);
-  };
-
-  const onPointerUp = (event: FederatedPointerEvent) => {
-    tool.current.onPointerUp(event);
-  };
-
-  const onPointerLeave = (event: FederatedPointerEvent) => {
-    tool.current.onPointerLeave(event);
-  };
 
   if (!canUseDOM) return;
 
+  const WIDTH = window.innerWidth - 200;
+  const HEIGHT = window.innerHeight - 200;
+  const HIT = new Rectangle(0, 0, WIDTH, HEIGHT);
+
   return (
-    <Application width={WIDTH} height={HEIGHT}>
-      <pixiContainer
-        hitArea={HIT}
-        eventMode="dynamic"
-        interactive={true}
-        onPointerDown={onPointerDown}
-        onPointerMove={onPointerMove}
-        onPointerUp={onPointerUp}
-        onPointerLeave={onPointerLeave}
-      >
-        {history.toArray().map((command, index) => {
-          // @TODO: Switch component based on command
-          return <Brush key={index} index={index}></Brush>;
-        })}
-      </pixiContainer>
-    </Application>
+    <pixiContainer width={WIDTH} height={HEIGHT}>
+      <pixiGraphics
+        draw={(g) => {
+          g.rect(
+            (window.innerWidth - WIDTH) / 2,
+            (window.innerHeight - HEIGHT) / 2,
+            WIDTH,
+            HEIGHT,
+          ).fill(0x111111);
+        }}
+      ></pixiGraphics>
+      {history.toArray().map((command, index) => {
+        // @TODO: Switch component based on command
+        return <Brush key={index} index={index}></Brush>;
+      })}
+    </pixiContainer>
   );
 }

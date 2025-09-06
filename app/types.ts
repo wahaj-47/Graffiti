712b245c6170857beb49012d69b853e0a7c9d8bd
Wahaj Hussain <wahajhssn@gmail.com>
2025-09-06 16:24:31 -0500

    Tool is now an abstract class

--- app/types.ts
+++ app/types.ts
@@ -1,17 +1,8 @@
 import type { FederatedPointerEvent } from "pixi.js";
-import type { Array, Map } from "yjs";
+import type { Array, Doc, Map } from "yjs";
 
 export interface ToolConfig {
   id: string;
-}
-
-export interface Tool {
-  readonly id: string;
-  getConfig(): ToolConfig;
-  onPointerDown(e: FederatedPointerEvent): Array<unknown>;
-  onPointerMove(e: FederatedPointerEvent): void;
-  onPointerUp(e: FederatedPointerEvent): void;
-  onPointerLeave(e: FederatedPointerEvent): void;
 }
 
 export type BrushProps = {

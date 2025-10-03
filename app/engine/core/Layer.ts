451ca3a0fe294adce9a1687e256c01e86d3bd62e
Wahaj Hussain <wahajhssn@gmail.com>
2025-10-02 23:00:52 -0500

    Disable caching

--- app/engine/core/Layer.ts
+++ app/engine/core/Layer.ts
@@ -5,15 +5,5 @@ interface LayerOptions extends ContainerOptions {}
 export class Layer extends Container {
   constructor(options: LayerOptions) {
     super(options);
-
-    this.cacheAsTexture({ antialias: true });
-
-    const ticker = new Ticker();
-    ticker.add(this.update, this);
-    ticker.start();
-  }
-
-  protected update(): void {
-    this.updateCacheTexture();
   }
 }

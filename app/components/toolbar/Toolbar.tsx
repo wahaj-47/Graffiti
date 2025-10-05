3b4ee807f13aaba4134bf32f336a8d46657f968e
Wahaj Hussain <wahajhssn@gmail.com>
2025-10-04 23:50:53 -0500

    A simple toolbar

--- app/components/toolbar/Toolbar.tsx
+++ app/components/toolbar/Toolbar.tsx
@@ -1,3 +1,28 @@
+import { useKeyPress } from "~/hooks/useKeys";
+import type { ToolConfig, ToolDefinition } from "~/types";
+import { PaintBrush } from "../tool/PaintBrush";
+import { Eraser } from "../tool/Eraser";
+
 export function Toolbar() {
-  return <div></div>;
+  return (
+    <div className="absolute left-px mt-2 ms-2 border border-zinc-600 rounded-sm">
+      <ToolButton tool={PaintBrush}></ToolButton>
+      <ToolButton tool={Eraser}></ToolButton>
+    </div>
+  );
+}
+
+type ToolButtonProps<T extends ToolConfig> = {
+  tool: ToolDefinition<T>;
+};
+
+function ToolButton<T extends ToolConfig>({ tool }: ToolButtonProps<T>) {
+  useKeyPress([tool.shortcut], (e) => {});
+
+  return (
+    <tool.Icon
+      className="rounded-xs p-2 not-last:border-b divide-y-reverse border-zinc-600 text-white"
+      size={40}
+    ></tool.Icon>
+  );
 }

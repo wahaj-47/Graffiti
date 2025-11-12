926c974721e3d3a0dc146a16162c3fb199ade306
Wahaj Hussain <wahajhssn@gmail.com>
2025-11-11 21:48:04 -0600

    Better GProps

--- app/engine/decorators/GProperty.ts
+++ app/engine/decorators/GProperty.ts
@@ -1,18 +1,38 @@
-type GPropertySpecifiers = {
+import type { Listener } from "~/types";
+
+export type GPropertySpecifiers<T, K extends keyof T> = {
+  propertyKey: K;
   type: "number" | "color";
   label: string;
+  get: () => T[K];
+  set: (value: T[K]) => void;
 };
 
-export function GProperty(options: GPropertySpecifiers) {
-  return function (target: any, key: string) {
+export function GProperty<T, K extends keyof T>(
+  options: Omit<GPropertySpecifiers<T, K>, "propertyKey" | "get" | "set">,
+) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
     if (!target.constructor.GProps) {
       target.constructor.GProps = [];
     }
 
-    target.constructor.GProps.push(options);
+    target.constructor.GProps.push({
+      ...options,
+      propertyKey: key,
+      get: descriptor.get,
+      set: descriptor.set,
+    });
   };
 }
 
-export function getGProps<T extends { constructor: any }>(instance: T): GPropertySpecifiers[] {
-  return instance.constructor.GProps ?? null;
+export function getGProps<T extends { constructor: any }>(instance: T): GPropertySpecifiers<T, keyof T>[] {
+  const GProps: GPropertySpecifiers<T, keyof T>[] = instance.constructor.GProps ?? [];
+
+  return GProps.map((prop) => {
+    return {
+      ...prop,
+      get: prop.get.bind(instance),
+      set: prop.set.bind(instance),
+    };
+  });
 }
